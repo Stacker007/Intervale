9447842eddf5bfa4527b8a894e76d6fe335f1ae1
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-19 20:43:06 +0300

    Create RPN.java

---
+++ RPN.java
@@ -0,0 +1,61 @@
+package com.company;
+
+/**
+ *
+ *
+ * Created by Student on 19.02.2018.
+ */
+
+import java.util.*;
+public class RPN {
+
+    private static String opn(String strInput) throws Exception{
+        StringBuilder sbStack = new StringBuilder("");
+        StringBuilder sbOut = new StringBuilder("");
+        char symbol, cTmp;
+
+        for (int i = 0; i < strInput.length(); i++) {
+            symbol = strInput.charAt(i);
+            if (isOp(symbol)) {
+                while (sbStack.length() > 0) {
+                    cTmp = sbStack.substring(sbStack.length()-1).charAt(0);
+                    if (isOp(cTmp) && (opPrior(cIn) <= opPrior(cTmp))) {
+                        sbOut.append(" ").append(cTmp).append(" ");
+                        sbStack.setLength(sbStack.length()-1);
+                    } else {
+                        sbOut.append(" ");
+                        break;
+                    }
+                }
+                sbOut.append(" ");
+                sbStack.append(cIn);
+            } else if ('(' == cIn) {
+                sbStack.append(cIn);
+            } else if (')' == cIn) {
+                cTmp = sbStack.substring(sbStack.length()-1).charAt(0);
+                while ('(' != cTmp) {
+                    if (sbStack.length() < 1) {
+                        throw new Exception("Ошибка разбора скобок. Проверьте правильность выражения.");
+                    }
+                    sbOut.append(" ").append(cTmp);
+                    sbStack.setLength(sbStack.length()-1);
+                    cTmp = sbStack.substring(sbStack.length()-1).charAt(0);
+                }
+                sbStack.setLength(sbStack.length()-1);
+            } else {
+                // Если символ не оператор - добавляем в выходную последовательность
+                sbOut.append(cIn);
+            }
+        }
+
+        // Если в стеке остались операторы, добавляем их в входную строку
+        while (sbStack.length() > 0) {
+            sbOut.append(" ").append(sbStack.substring(sbStack.length()-1));
+            sbStack.setLength(sbStack.length()-1);
+        }
+
+        return  sbOut.toString();
+    }
+
+
+}
