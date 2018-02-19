845641b5d57637be5157a76a29b8f95d7a165191
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-19 21:14:54 +0300

    Update RPN.java

--- RPN.java
+++ RPN.java
@@ -1,47 +1,47 @@
 package com.company;
 
 /**
- *
+ * Класс приведения выражения к виду оратной польской нотации
  *
  * Created by Student on 19.02.2018.
  */
 
-import java.util.*;
+
 public class RPN {
 
     private static String opn(String strInput) throws Exception{
-        StringBuilder sbStack = new StringBuilder("");
+        StringBuilder rpnSt = new StringBuilder(""); //
         StringBuilder sbOut = new StringBuilder("");
         char symbol, cTmp;
 
         for (int i = 0; i < strInput.length(); i++) {
             symbol = strInput.charAt(i);
             if (isOp(symbol)) {
-                while (sbStack.length() > 0) {
-                    cTmp = sbStack.substring(sbStack.length()-1).charAt(0);
+                while (rpnSt.length() > 0) {
+                    cTmp = rpnSt.substring(rpnSt.length()-1).charAt(0);
                     if (isOp(cTmp) && (opPrior(cIn) <= opPrior(cTmp))) {
                         sbOut.append(" ").append(cTmp).append(" ");
-                        sbStack.setLength(sbStack.length()-1);
+                        rpnSt.setLength(rpnSt.length()-1);
                     } else {
                         sbOut.append(" ");
                         break;
                     }
                 }
                 sbOut.append(" ");
-                sbStack.append(cIn);
+                rpnSt.append(cIn);
             } else if ('(' == cIn) {
-                sbStack.append(cIn);
+                rpnSt.append(cIn);
             } else if (')' == cIn) {
-                cTmp = sbStack.substring(sbStack.length()-1).charAt(0);
+                cTmp = rpnSt.substring(rpnSt.length()-1).charAt(0);
                 while ('(' != cTmp) {
-                    if (sbStack.length() < 1) {
+                    if (rpnSt.length() < 1) {
                         throw new Exception("Ошибка разбора скобок. Проверьте правильность выражения.");
                     }
                     sbOut.append(" ").append(cTmp);
-                    sbStack.setLength(sbStack.length()-1);
-                    cTmp = sbStack.substring(sbStack.length()-1).charAt(0);
+                    rpnSt.setLength(rpnSt.length()-1);
+                    cTmp = rpnSt.substring(rpnSt.length()-1).charAt(0);
                 }
-                sbStack.setLength(sbStack.length()-1);
+                rpnSt.setLength(rpnSt.length()-1);
             } else {
                 // Если символ не оператор - добавляем в выходную последовательность
                 sbOut.append(cIn);
@@ -49,9 +49,9 @@ public class RPN {
         }
 
         // Если в стеке остались операторы, добавляем их в входную строку
-        while (sbStack.length() > 0) {
-            sbOut.append(" ").append(sbStack.substring(sbStack.length()-1));
-            sbStack.setLength(sbStack.length()-1);
+        while (rpnSt.length() > 0) {
+            sbOut.append(" ").append(rpnSt.substring(rpnSt.length()-1));
+            rpnSt.setLength(rpnSt.length()-1);
         }
 
         return  sbOut.toString();
