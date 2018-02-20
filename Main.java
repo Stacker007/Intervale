b3a4a911a70f5d40b866c20ece40ee271a4fa53f
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-20 04:38:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,39 +6,42 @@ public class Main{
 
 
     private static final String PATHIN = "D:/Petr/Calc/Tmp/input_1.txt";
-  // private static final String PATHOUT = "D:/Petr/Calc/Tmp/output_1.txt";
-    //private static InOutOfFile outputRow;*/
+    // private static final String PATHOUT = "D:/Petr/Calc/Tmp/output_1.txt";
+
     public static void main(String[] args) throws IOException{
 
         try {
             File file = new File(PATHIN);
             FileReader fr = new FileReader(file);
             BufferedReader reader = new BufferedReader(fr);
-            String line = reader.readLine();
-            System.out.println(line);
 
-           /* while (line != null) {
-                System.out.println(line);
-                // считываем остальные строки в цикле
+            String line = reader.readLine();
+            String lineRPN = line;
+            while (line != null){
+            RPN lineRpn = new RPN(line);
+            double dResult=0;
+
+            try {
+                lineRPN = lineRpn.rpn(); //Приводим строку к виду обратной нотации
+                Simplecalc result = new Simplecalc(lineRPN);
+                dResult=result.calculate();
+
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+
+            System.out.print(lineRPN);
+                System.out.println("::"+line+"="+dResult);
 
 
                 line = reader.readLine();
-            }*/
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-       /* inputRow = new InOutOfFile(PATHIN);
-        outputRow = new InOutOfFile(PATHOUT);
-        inputRow.read();
-        outputRow.write("ghbdtn");
-
-        //System.out.println(inputRow);
-
-*/
 
     }
 
-
- }
+}
