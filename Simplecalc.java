0b8ce4b682428aa81175bfd39c067b5b62dcef4d
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-20 19:54:40 +0300

    Update Simplecalc.java

--- Simplecalc.java
+++ Simplecalc.java
@@ -5,45 +5,34 @@ public class Simplecalc {
     Simplecalc (String strInput){
         this.strInput=strInput;    }
 
+
     public double calculate() throws Exception {
         double a = 0, b = 0;
-        String sTmp;
+        String lexemTmp;
         Deque<Double> stack = new ArrayDeque<Double>();
         StringTokenizer st = new StringTokenizer(strInput);
-        while(st.hasMoreTokens()) {
+        while (st.hasMoreTokens()){ //В этом классе используются только три лексемы (2 операнда и 1 оператор)
             try {
-                sTmp = st.nextToken().trim();
-                if (1 == sTmp.length() && operator(sTmp.charAt(0))) {
-                    if (stack.size() < 2) {
-                        throw new Exception("Неверное количество данных в стеке для операции " + sTmp);
+                lexemTmp = st.nextToken().trim();
+                if (1 == lexemTmp.length() && operator(lexemTmp.charAt(0))) { //
+                    if (stack.size() != 2 ) {
+                        throw new Exception("Количество данных неверно " + lexemTmp);
                     }
                     b = stack.pop();
                     a = stack.pop();
-                    switch (sTmp.charAt(0)) {
-                        case '+':
-                            a += b;
-                            break;
-                        case '-':
-                            a -= b;
-                            break;
-                        case '/':
-                            a /= b;
-                            break;
-                        case '*':
-                            a *= b;
-                            break;
-                        case '%':
-                            a = a*b/100;
-                            break;
-                        case '^':
-                            a = Math.pow(a, b);
-                            break;
+                    switch (lexemTmp.charAt(0)) {
+                        case '+': a += b; break;
+                        case '-': a -= b; break;
+                        case '/': a /= b; break;
+                        case '*': a *= b; break;
+                        case '%': a = a*b/100; break;
+                        case '^': a = Math.pow(a, b); break;
                         default:
-                            throw new Exception("Недопустимая операция " + sTmp);
+                            throw new Exception("Операция не обрабатывается этим типом калькулятора " + lexemTmp);
                     }
-                    stack.push(a);
+                    stack.push(a); // возврат в стек результата вычисления
                 } else {
-                    a = Double.parseDouble(sTmp);
+                    a = Double.parseDouble(lexemTmp);
                     stack.push(a);
                 }
             } catch (Exception e) {
