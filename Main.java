1769bf06ef01d5fcd90622bb21a3861b749653ef
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-21 23:24:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,19 +2,17 @@ package com.company;
 
 import java.io.*;
 
-public class Main{
+public class Main {
 
     private static final String PATHINSIMP = "d:/b/input_1.txt";
     private static final String PATHOUTSIMP = "d:\\b\\output_1.txt";
     private static final String PATHIN = "d:/b/input_2.txt";
     private static final String PATHOUT = "d:\\b\\output_2.txt";
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
         CalulateFORSimple.letSimpCalc(PATHINSIMP, PATHOUTSIMP);
         CalulateFORSimple.letCalc(PATHIN, PATHOUT);
-
-
 
     }
 
