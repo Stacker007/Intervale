7c1823c8c29334adc3434a38056e6ab192df6cb1
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-21 19:19:17 +0300

    Update Simplecalc.java

--- Simplecalc.java
+++ Simplecalc.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Simplecalc {
     String strInput;
     Simplecalc(){
-        
+        System.out.println("Пустой аргумент калькулятора");
     }
     Simplecalc (String stInput){
         strInput = stInput;
@@ -30,20 +30,38 @@ class Simplecalc {
         try {
             lexemTmp = st.nextToken().trim();
             if (1 == lexemTmp.length() && operator(lexemTmp.charAt(0))) { //
-                if (stack.size() != 2 ) {
+                if (stack.size() < 2 ) {
                     throw new Exception("Количество данных неверно " + lexemTmp);
                 }
                 b = stack.pop();
                 a = stack.pop();
-                switch (lexemTmp.charAt(0)) {
-                    case '+': a += b; break;
-                    case '-': a -= b; break;
-                    case '/': a /= b; break;
-                    case '*': a *= b; break;
-                    case '%': a = a*b/100; break;
-                    case '^': a = Math.pow(a, b); break;
-                    default:
-                        throw new Exception("Операция не обрабатывается этим типом калькулятора " + lexemTmp);
+                try {
+
+
+                    switch (lexemTmp.charAt(0)) {
+                        case '+':
+                            a += b;
+                            break;
+                        case '-':
+                            a -= b;
+                            break;
+                        case '/':
+                            a /= b;
+                            break;
+                        case '*':
+                            a *= b;
+                            break;
+                        case '%':
+                            a = a * b / 100;
+                            break;
+                        case '^':
+                            a = Math.pow(a, b);
+                            break;
+                        default:
+                            throw new Exception("Операция не обрабатывается этим типом калькулятора " + lexemTmp);
+                    }
+                }catch (ArithmeticException e){
+                    System.out.println("На ноль делить нельзя!");
                 }
                 stack.push(a); // возврат в стек результата вычисления
             } else {
