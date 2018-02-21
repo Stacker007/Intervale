fcda48f5059aa7eb42e44936a27478e6989be902
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-21 21:15:03 +0300

    Update CalculateFORSimple..java

--- CalculateFORSimple..java
+++ CalculateFORSimple..java
@@ -1,15 +1,19 @@
 package com.company;
-
+import java.text.DecimalFormat;
 import java.io.*;
 
 /**
  * Created by Student on 21.02.2018.
  */
-public class CalulateFORSimple  {
+public class CalulateFORSimple {
 
-    public static void letCalc (String pathIn, String pathOut)
+
+
+    public static void letSimpCalc(String pathIn, String pathOut)
 
     {
+
+
         try {
             File file = new File(pathIn);
             FileReader fr = new FileReader(file);
@@ -17,8 +21,7 @@ public class CalulateFORSimple  {
 
             String line = reader.readLine();
             String lineRPN = line;
-            try(FileWriter writer = new FileWriter(pathOut, false))
-            {
+            try (FileWriter writer = new FileWriter(pathOut, false)) {
                 writer.write("");
 
                 writer.flush();
@@ -27,10 +30,7 @@ public class CalulateFORSimple  {
             while (line != null) {
                 RPN lineRpn = new RPN(line);
                 double dResult = 0;
-                String err="", result = "";
-
-
-
+                String err = "", result = "";
                 try {
                     lineRPN = lineRpn.rpn(); //Приводим строку к виду обратной нотации
                     Simplecalc simpCalc = new Simplecalc(lineRPN);
@@ -40,23 +40,16 @@ public class CalulateFORSimple  {
                 } catch (Exception e) {
                     err = e.getMessage();
                 }
-                if (err == "") {
-                    System.out.println(line + "=" + lineRPN);
-                }
+                try (FileWriter writer = new FileWriter(pathOut, true)) {
+                    String text = line + "=" + result ;
+                    if (err == "") writer.write(text + "\r\n");
 
-                try(FileWriter writer = new FileWriter(pathOut, true))
-                {
-
-                    String text = line + "=" + result+"\n";
-                    writer.write(text+"\n");
-
-
-
+                    writer.write(line + err);
                     writer.flush();
-                }
-                catch(IOException ex){
+                    writer.close();
 
-                    System.out.println(ex.getMessage());
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
 
                 line = reader.readLine();
@@ -68,5 +61,67 @@ public class CalulateFORSimple  {
         }
     }
 
+    public static void letCalc(String pathIn, String pathOut)
+
+    {
+        try {
+            File file = new File(pathIn);
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+
+            String line = reader.readLine();
+            String lineRPN = line;
+            try (FileWriter writer = new FileWriter(pathOut, false)) {
+                writer.write("");
+
+                writer.flush();
+            }
+
+            while (line != null) {
+                RPN lineRpn = new RPN(line);
+                double dResult = 0;
+                String err = "", result = "";
+                try {
+                    lineRPN = lineRpn.rpn(); //Приводим строку к виду обратной нотации
+                    Calc simpCalc = new Calc(lineRPN);
+                    dResult = simpCalc.calculate(); //передаем строку методу из класса Simlecalculate
+
+                    result = String.format("%.5f", dResult);
+                   result.replace(',','.');
+                    /*boolean flag=true;
+                    for (int i=result.length(); i>result.length()-6;i++){
+                        if (result.charAt(i)!=0) flag = false;
+                    }
+                    if (flag) {
+                        String  tmp="";
+                        for (int i = 0; i < result.length() - 6; i++) {
+                            tmp+=result.charAt(i);
+                        }
+                        result=tmp;
+                    }*/
+
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                try (FileWriter writer = new FileWriter(pathOut, true)) {
+                    String text = line + "=" + result;
+                    if (err == "") writer.write(text + "\r\n");
+                    writer.write(err);
+                    writer.flush();
+                    writer.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
 
