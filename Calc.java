aac7b3082249eac57c0661f39c2c87e4324c4ff8
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-21 07:03:47 +0300

    Create Calc.java

---
+++ Calc.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+class Calc extends Simplecalc {
+    Calc (String st){
+        strInput = st;
+    }
+
+    public double calculate() throws Exception {
+        double a = 0, b = 0;
+
+        Deque<Double> stack = new ArrayDeque<Double>();
+        StringTokenizer st = new StringTokenizer(strInput);
+        while (st.hasMoreTokens()){
+            justCalc(st, stack, a, b);
+        }
+        return stack.pop();
+    }
+
+}
