b70eeabfed5edb6c1a8c26733c5ac3dd916146c9
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-21 21:15:20 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,16 @@ import java.io.*;
 
 public class Main{
 
-
     private static final String PATHINSIMP = "d:/b/input_1.txt";
     private static final String PATHOUTSIMP = "d:\\b\\output_1.txt";
+    private static final String PATHIN = "d:/b/input_2.txt";
+    private static final String PATHOUT = "d:\\b\\output_2.txt";
 
     public static void main(String[] args) throws IOException{
 
-        CalulateFORSimple.letCalc(PATHINSIMP, PATHOUTSIMP);
+        CalulateFORSimple.letSimpCalc(PATHINSIMP, PATHOUTSIMP);
+        CalulateFORSimple.letCalc(PATHIN, PATHOUT);
+
 
 
     }
