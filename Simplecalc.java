dfa075cfdeec9954a50cef75665bb304c5e600c8
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-21 07:02:43 +0300

    Update Simplecalc.java

--- Simplecalc.java
+++ Simplecalc.java
@@ -1,50 +1,59 @@
 package com.company;
 import java.util.*;
-public class Simplecalc {
-    private String strInput;
-    Simplecalc (String strInput){
-        this.strInput=strInput;    }
+class Simplecalc {
+    String strInput;
+    Simplecalc(){
+        
+    }
+    Simplecalc (String stInput){
+        strInput = stInput;
+    }
 
 
     public double calculate() throws Exception {
         double a = 0, b = 0;
-        String lexemTmp;
+
         Deque<Double> stack = new ArrayDeque<Double>();
         StringTokenizer st = new StringTokenizer(strInput);
-        while (st.hasMoreTokens()){ //В этом классе используются только три лексемы (2 операнда и 1 оператор)
-            try {
-                lexemTmp = st.nextToken().trim();
-                if (1 == lexemTmp.length() && operator(lexemTmp.charAt(0))) { //
-                    if (stack.size() != 2 ) {
-                        throw new Exception("Количество данных неверно " + lexemTmp);
-                    }
-                    b = stack.pop();
-                    a = stack.pop();
-                    switch (lexemTmp.charAt(0)) {
-                        case '+': a += b; break;
-                        case '-': a -= b; break;
-                        case '/': a /= b; break;
-                        case '*': a *= b; break;
-                        case '%': a = a*b/100; break;
-                        case '^': a = Math.pow(a, b); break;
-                        default:
-                            throw new Exception("Операция не обрабатывается этим типом калькулятора " + lexemTmp);
-                    }
-                    stack.push(a); // возврат в стек результата вычисления
-                } else {
-                    a = Double.parseDouble(lexemTmp);
-                    stack.push(a);
-                }
-            } catch (Exception e) {
-                throw new Exception("Недопустимый символ в выражении");
-            }
+        for (int i = 0; i < 3; i++){ //В этом классе используются только три лексемы (2 операнда и 1 оператор)
+            justCalc(st, stack, a, b);
         }
-
-        if (stack.size() > 1) {
-            throw new Exception("Количество операторов не соответствует количеству операндов");
+        if (st.hasMoreTokens()) {
+            throw new Exception(": Этот тип калькулятора обрабатывает только простые выражения");
         }
 
         return stack.pop();
+    }
+
+    protected static void justCalc (StringTokenizer st, Deque<Double> stack, double a, double b){
+        String lexemTmp;
+        try {
+            lexemTmp = st.nextToken().trim();
+            if (1 == lexemTmp.length() && operator(lexemTmp.charAt(0))) { //
+                if (stack.size() != 2 ) {
+                    throw new Exception("Количество данных неверно " + lexemTmp);
+                }
+                b = stack.pop();
+                a = stack.pop();
+                switch (lexemTmp.charAt(0)) {
+                    case '+': a += b; break;
+                    case '-': a -= b; break;
+                    case '/': a /= b; break;
+                    case '*': a *= b; break;
+                    case '%': a = a*b/100; break;
+                    case '^': a = Math.pow(a, b); break;
+                    default:
+                        throw new Exception("Операция не обрабатывается этим типом калькулятора " + lexemTmp);
+                }
+                stack.push(a); // возврат в стек результата вычисления
+            } else {
+                a = Double.parseDouble(lexemTmp);
+                stack.push(a);
+            }
+        } catch (Exception e) {
+            //throw new Exception("Недопустимый символ в выражении");
+        }
+
     }
     private static boolean operator (char op) {
         switch (op) {
